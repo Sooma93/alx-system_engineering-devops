b1cc254f4ff4578613f4262ade244b4dedaf6c74
sooma93 <tasneemabasit5@gmail.com>
2024-02-23 14:07:46 -0800

    d

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# 1-install_a_package.pp
-
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
-package { 'Flask':
+# Using Puppet, install flask from pip3
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-
