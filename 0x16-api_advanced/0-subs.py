a200ecf9447ac4b791146e8ab4de7a8988b1bc8f
sooma93 <tasneemabasit5@gmail.com>
2024-04-17 10:16:57 -0700

    d

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 """
-api redient
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
 """
-
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-    function redor=ent query
-    """
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': u_agent
+    }
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url,  headers={"User-Agent": "Custom"})
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data.get('data').get('subscribers')
-        return subscribers
-    else:
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
